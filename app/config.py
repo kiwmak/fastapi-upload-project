587404a5457928c1cd214a170b6fed5ba4031643
kiwmak <131851318+kiwmak@users.noreply.github.com>
2025-12-11 12:22:59 +0700

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,10 +1,10 @@
+import os
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "FastAPI Image Upload Service"
-    # Đường dẫn nơi ảnh sẽ được lưu trữ (tương đối với gốc dự án)
     UPLOAD_DIR: str = "app/static/uploads"
-    # Cấu hình Database (dùng SQLite cho đơn giản)
-    DATABASE_URL: str = "sqlite:///./sql_app.db"
+    # Railway sẽ cung cấp biến DATABASE_URL nếu bạn add plugin Postgres
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///./sql_app.db")
 
 settings = Settings()
